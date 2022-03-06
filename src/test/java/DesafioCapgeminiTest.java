4d2a98c750f7ea781523126c5d775042924f889c
Weslley do Cati Ferreira <weslley.docati@gmail.com>
2022-03-06 19:12:52 -0300

    Adicionado o teste 2 da questão 3

--- src/test/java/DesafioCapgeminiTest.java
+++ src/test/java/DesafioCapgeminiTest.java
@@ -16,15 +16,21 @@ public class DesafioCapgeminiTest {
     @DisplayName("Questão 2: total de 3 pares com diferença de de 2 na lista [1, 5, 3, 4, 2]")
     void testQuestao2(){
         int[] array = {1, 5, 3, 4, 2};
-        assertEquals(3, Questao2.totalPares(array, 2), "Deve conter 3 pares com diferença de de 2 na lista [1, 5, 3, 4, 2]");
+        assertEquals(3, Questao2.totalPares(array, 2), "Total de 3 pares com diferença de de 2 na lista [1, 5, 3, 4, 2]");
     }
 
     @Test
-    @DisplayName("Questão 3: Encriptar 'tenha um bom dia' para 'taoa eum nmd hbi'")
-    void testQuestao3(){
+    @DisplayName("Questão 3 - Teste 1: Encriptar 'tenha um bom dia' para 'taoa eum nmd hbi'")
+    void test1Questao3(){
         String s = "tenha um bom dia";
-        assertEquals("taoa eum nmd hbi", Questao3.encriptar(s), "deve encriptar 'tenha um bom dia' para 'taoa eum nmd hbi'");
+        assertEquals("taoa eum nmd hbi", Questao3.encriptar(s), "Encriptar 'tenha um bom dia' para 'taoa eum nmd hbi'");
     }
 
+    @Test
+    @DisplayName("Questão 3 - Teste 2: Encriptar '' para 'omd luo an'")
+    void test2Questao3(){
+        String s = "ola mundo";
+        assertEquals("omd luo an", Questao3.encriptar(s), "Encriptar 'tenha um bom dia' para 'taoa eum nmd hbi'");
+    }
 
 }
